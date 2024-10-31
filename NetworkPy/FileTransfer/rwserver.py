a665eb4fda673d2051a090ac3641b6df8ac63564
Bernard Black <chronophob@gmail.com>
2024-10-31 15:40:20 +0100

    Added directory path creation on rwserver

--- NetworkPy/FileTransfer/rwserver.py
+++ NetworkPy/FileTransfer/rwserver.py
@@ -16,8 +16,18 @@ def upload_file():
   if file.filename == '':
     return jsonify({'message': 'No file selected'}), 400
 
-  file.save(os.path.join(UPLOAD_FOLDER, file.filename))
-  return jsonify({'message': f'File {file.filename} uploaded successfully'}), 200
+  # Retrieve the relative path from the request form data
+  relative_path = request.form.get('relative_path', file.filename)
+  normalized_path = relative_path.replace('\\', '/')  # Convert Windows backslashes to Unix
+
+  # Construct the full path by joining the UPLOAD_FOLDER with the normalized path
+  full_path = os.path.join(UPLOAD_FOLDER, normalized_path)
+
+  # Create any necessary subdirectories
+  os.makedirs(os.path.dirname(full_path), exist_ok=True)
+
+  file.save(full_path)
+  return jsonify({'message': f'File {normalized_path} uploaded successfully'}), 200
 
 if __name__ == '__main__':
-  app.run(host='192.168.1.150', port=9999)
+  app.run(host='192.168.10.150', port=9999)
