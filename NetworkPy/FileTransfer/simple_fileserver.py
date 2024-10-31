bdea42ad1f9b4c13363fdbcd6e85afdf282b7b78
Bernard Black <chronophob@gmail.com>
2024-10-31 17:08:33 +0100

    Added user input filepath for simple_fileserver

--- NetworkPy/FileTransfer/simple_fileserver.py
+++ NetworkPy/FileTransfer/simple_fileserver.py
@@ -1,27 +1,40 @@
 import http.server
 import socketserver
+import socket
 import os
-
-PORT = 9999
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
   def end_headers(self):
     self.send_header('Access-Control-Allow-Origin', '*')
     super().end_headers()
 
-os.chdir('/home/bernard/Programming/Python/ITHS/')
-handler_object = MyHttpRequestHandler
+def run_fileserver(serve_path):
+  os.chdir(serve_path)
+  handler_object = MyHttpRequestHandler
 
-with socketserver.TCPServer(("", PORT), handler_object) as httpd:
-  print(f'Serving at port {PORT}')
-  print("Server is running. Press Ctrl+C to stop the server.")
+  with socketserver.TCPServer(("", PORT), handler_object) as httpd:
+    print(f'Serving {serve_path} at port {PORT}')
+    print("Server is running. Press Ctrl+C to stop the server.")
 
-  try:
-    httpd.serve_forever()
-  except KeyboardInterrupt:
-    pass
+    try:
+      httpd.serve_forever()
+    except KeyboardInterrupt:
+      pass
 
-  print("Server stopped.")
-  httpd.server_close()
-  print("Server closed.")
+    print("Server stopped.")
+    httpd.server_close()
+    print("Server closed.")
+
+if __name__ == '__main__':
+  PORT = 9999
+  while True:
+    SERVE_PATH = input("Enter the path you want to serve (leave empty for cwd):\n")
+    if not SERVE_PATH:
+      SERVE_PATH = os.getcwd()
+    if os.path.exists(SERVE_PATH):
+      run_fileserver(SERVE_PATH)
+      exit(0)
+    else:
+      print("Path does not exist. Make sure path exists")
+
 
