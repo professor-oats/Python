b592d0358cf0d371cb80cf4207d3ee6b1aaa00bc
professor-oats <chronophob@gmail.com>
2024-11-02 23:45:27 +0100

    Update word_replacor.py to default Windows carrier return replace
    Put it in there

--- WorkTools/word_replacor.py
+++ WorkTools/word_replacor.py
@@ -83,9 +83,9 @@ def main():
   # Set up argparse for command-line arguments
   parser = argparse.ArgumentParser(description="Replace regex matches in files within a directory."
                                                "Enclose the match in quotes"
-                                   "Default: regexmatch r'\r' (carriage return), replace_text=''")
+                                   "Default: regexmatch r'\r' (carriage return), replace_text='\n'")
   parser.add_argument("directory", help="Path to the directory or a single file to search in")
-  parser.add_argument("regex_pattern", nargs='?', default=r'\', help="The regex pattern to search for")
+  parser.add_argument("regex_pattern", nargs='?', default=r'\^M', help="The regex pattern to search for")
   parser.add_argument("replacement_text", nargs='?', default='\n', help="The text to replace the pattern with")
   parser.add_argument("-r", action="store_true", help="Enable regex replacement mode:"
                                                       "'text_to_match' 'text_to_replace_with' (Leave empty replace for remove)")
